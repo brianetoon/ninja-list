dce9980246ba9cf9127e66e7a31b1259de90bd68
Brian <brian.elliot.toon@gmail.com>
2022-11-11 19:13:47 -0500

    linking between pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
          <h1>Homepage</h1>
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis voluptatem alias obcaecati rerum dolorum ab aliquid culpa hic, optio, fugit debitis velit ipsam. Architecto at commodi recusandae earum repudiandae modi?</p>
          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, harum deleniti magni quo sequi consequuntur dolorem voluptatum. Porro, temporibus magnam voluptatum, eligendi dignissimos sed quis omnis modi quia distinctio optio.</p>
+         <Link href="/ninjas">See Ninja Listing</Link>
          <Footer />
       </div>
    )
