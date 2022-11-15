bb809a7ebbd92ad1adbc3fd96e8c430505d49431
Brian <brian.elliot.toon@gmail.com>
2022-11-14 19:45:50 -0500

    redirecting users

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,14 +1,34 @@
 import styles from '../styles/NotFound.module.css'
-import Link from "next/link";
+import Link from "next/link"
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 const NotFound = () => {
-    return (  
-        <div className={styles["not-found"]}>
-            <h2>Ooops...</h2>
-            <p className="error">That page could not be found.</p>
-            <p>Go back <Link href="/">Home</Link></p>
-        </div>
-    );
+   const router = useRouter()
+   const [redirectTime, setRedirectTime] = useState(5)
+
+   useEffect(() => {
+      setTimeout(() => {
+
+         if (redirectTime > 0) {
+            setRedirectTime(redirectTime - 1)
+         }
+
+         if (redirectTime <= 0) {
+            router.push('/')
+         }
+
+      }, 1000)
+   }, [redirectTime])
+
+   return (  
+      <div className={styles["not-found"]}>
+         <h2>Ooops...</h2>
+         <p className="error">That page could not be found.</p>
+         <p>Redirecting in { redirectTime } seconds...</p>
+         <p>Go back <Link href="/">Home</Link> now.</p>
+      </div>
+   );
 }
  
 export default NotFound;
